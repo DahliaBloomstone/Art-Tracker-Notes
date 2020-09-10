6d2fc89a8e2481d6cc92c110311bbf7d95846cc6
github username <github email address>
2020-09-10 14:49:18 -0400

    users and sessions controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-end
-
-
-#hello 
+    protect_from_forgery with: :exception
+    include ApplicationHelper
+  
+  private
+  
+    def redirect_if_not_logged_in
+      if !logged_in?
+        flash[:danger] = "Please log in to Art Tracker!"
+        redirect_to '/'
+      end
+    end
+  end
