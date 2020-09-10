e7b997fb56f67aa4c289db0bf10be48f0c284b44
github username <github email address>
2020-09-10 14:41:56 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
+
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+  get '/auth/facebook/callback' => 'sessions#fbcreate'
+
+
+  resources :users
+  resources :art_plans
+  resources :art_projects, only: [:edit, :index, :show ]
+  resources :art_schedules
+
+  resources :art_plans do
+    resources :art_schedules, only: [:new, :index, :create]
+  end
+
+  resources :art_projects do
+    resources :art_schedules, only: [:new, :index, :create]
+  end
+end
+
 end
