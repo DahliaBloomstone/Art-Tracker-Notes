482d5d428bf42981bb519c6f2040d7590f4185ac
github username <github email address>
2020-09-11 13:59:38 -0400

    index code in art projects controller

--- app/controllers/art_projects_controller.rb
+++ app/controllers/art_projects_controller.rb
@@ -2,7 +2,12 @@ class ArtProjectsController < ApplicationController
     before_action :set_art_project, only: [:show, :edit, :update]
 
     def index 
-    end 
+    if params[:art_plan_id] && artplan = ArtPlan.find_by_id(params[:art_plan_id])
+    @art_projects = artplan.art_projects.paginate(page: params[:page], per_page: 2)
+    else
+     @art_projects = current_user.art_projects.paginate(page: params[:page], per_page: 2)
+     end
+end 
 
     def show 
     end 
