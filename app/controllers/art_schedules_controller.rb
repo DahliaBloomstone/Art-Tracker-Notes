6200b311c45f3a31982cf6278886ebeeb02fac3c
github username <github email address>
2020-09-11 15:36:56 -0400

    added crud actions to art schedules controller

--- app/controllers/art_schedules_controller.rb
+++ app/controllers/art_schedules_controller.rb
@@ -12,9 +12,23 @@ class ArtSchedulesController < ApplicationController
     end 
 
     def new 
+        if current_user && params[:art_plan_id] && @artplan = ArtPlan.find_by_id(params[:art_plan_id])
+     @art_schedule = @artplan.art_schedules.build
+     @art_schedule.build_art_project
+         else
+     @art_schedule = ArtSchedule.new
+     @art_schedule.build_art_project
+        end
     end 
 
     def create
+        @art_schedule = ArtSchedule.new(art_schedule_params)
+      if @art_schedule.save
+        flash[:success] = "Your art project schedule has been created! Start Working!"
+        redirect_to art_schedule_path(@art_schedule)
+      else
+        render :new
+      end
     end 
 
     def show
@@ -24,20 +38,40 @@ class ArtSchedulesController < ApplicationController
     end 
 
     def update
+   if @art_schedule.update(art_schedule_params)
+     flash[:success] = "Your art project schedule has been updated!"
+     redirect_to art_schedule_path(@art_schedule)
+     else
+  render :edit
+        end
     end 
 
     def destroy 
+    @art_schedule.destroy
+    flash[:success] = "You have successfully deleted this art schedule!"
+    redirect_to art_schedules_path
     end 
 
-    params 
+    private  
 
     def art_schedule_params
+    params.require(:art_schedule).permit(:art_time, :art_type, :art_id, :art_plan_id, art_attributes: [:medium, :idea, :artist_reference, :day, :content, :price])
     end 
 
     def set_art_schedules 
+        @art_schedule = ArtSchedule.find_by(id: params[:id])
+        if @art_schedule.nil?
+          flash[:danger] = "This Art Project Schedule is not Found! Try Something Else!"
+          redirect_to art_schedules_path
+        end
     end 
 
     def require_same_user 
+        set_art_schedules
+        if current_user.id != @art_schedule.art_plan.user_id
+          flash[:danger] = "You can only edit or delete your own art project schedule"
+          redirect_to art_schedules_path
+      end
     end 
     
 end 
